cda23898dd53fd76428152df193d589571997042
Victory <git@dfhu.org>
2015-05-14 20:00:46 -0400

    create a self stopping interval runner.

---
+++ app/src/main/java/org/dfhu/clicknrecord/SelfStoppingIntervalRunnable.java
@@ -0,0 +1,37 @@
+package org.dfhu.clicknrecord;
+
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SelfStoppingIntervalRunnable implements Runnable {
+    private final Runnable delegated;
+    private volatile ScheduledFuture<?> todo;
+    public AtomicBoolean shouldStop = new AtomicBoolean(false);
+    public SelfStoppingIntervalRunnable (Runnable r) {
+        delegated = r;
+    }
+    @Override
+    public void run() {
+        if (shouldStop()) {
+            todo.cancel(true);
+        }
+        delegated.run();
+    }
+
+    private void setShouldStop(boolean shouldStop) {
+        this.shouldStop.set(shouldStop);
+    }
+
+    private boolean shouldStop() {
+        return false;
+    }
+
+    public void scheduleAtFixedRate(
+            ScheduledExecutorService executor, long delay, long period, TimeUnit unit) {
+        todo = executor.scheduleAtFixedRate(this, delay, period, unit);
+    }
+
+}
